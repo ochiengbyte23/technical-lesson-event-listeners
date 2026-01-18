28f0b0067a11a02f94d2d7e5f57da53fa928997c
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-18 23:36:24 +0300

    added an event handler function

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 function Question() {
+  function handleClick() {
+    console.log("clicked")
+  }
   return (
     <>
       <h1> QUESTION HERE </h1>
-      <button onClick={() => console.log("Click")}> Answer 1</button>
-      <button onClick={() => console.log("Click")}> Answer 2</button>
-      <button onClick={() => console.log("Click")}> Answer 3</button>
-      <button onClick={() => console.log("Click")}> Answer 4</button>
+      <button onClick={() => handleClick()}> Answer 1</button>
+      <button onClick={handleClick}> Answer 2</button>
+      <button onClick={handleClick}> Answer 3</button>
+      <button onClick={handleClick}> Answer 4</button>
     </>
   );
 }
